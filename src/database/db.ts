5b61c6699e8eb235a509cb6a766cb96f6a3ab77f
sam chris <vsgitsam25@gmail.com>
2026-01-19 16:24:37 +0300

    small changes here and there

--- src/database/db.ts
+++ src/database/db.ts
@@ -2,11 +2,17 @@ import mongoose from "mongoose";
 
 async function connectDB() {
   try {
-    await mongoose.connect(process.env.MONGODB_URI!);
+    const mongoURI = process.env.MONGODB_URI;
+
+    if (!mongoURI) {
+      throw new Error("Mongodb uri environment variable is not defined");
+    }
+
+    await mongoose.connect(mongoURI);
     console.log("MongoDB connected successfully");
   } catch (error) {
     console.log("Could not connect to db");
-    console.log(error);
+    console.error(error);
     process.exit(1);
   }
 }
