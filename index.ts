0741afcfe154b3a28172603ba71aeda02ee6532f
sam chris <vsgitsam25@gmail.com>
2026-01-19 16:08:44 +0300

    added some catch in the index

--- index.ts
+++ index.ts
@@ -7,8 +7,13 @@ import connectDB from "./src/database/db.ts";
 
 const PORT = process.env.PORT || 3000;
 
-connectDB().then(() => {
-  app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+connectDB()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.error("Failed to start server", error);
+    process.exit(1);
   });
-});
